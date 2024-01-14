2a1945d115b5e4cb00c310eb00fdf0e33fde257f
nialliv <artemjevnikita@yandex.ru>
2024-01-14 11:36:30 +0400

    init commit

---
+++ src/main/java/ru/artemev/leetcode/RunnerTask.java
@@ -0,0 +1,17 @@
+package ru.artemev.leetcode;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RunnerTask {
+    @PostConstruct
+    public void init() {
+
+    }
+
+}
---
+++ src/test/java/ru/artemev/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artemev.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
