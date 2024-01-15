73e4abcca597d91ecc287b55775502cb1f319722
nialliv <artemjevnikita@yandex.ru>
2024-01-15 20:03:48 +0400

    added palindrome

---
+++ src/main/java/ru/artemev/leetcode/tasks/Palindrome.java
@@ -0,0 +1,19 @@
+package ru.artemev.leetcode.tasks;
+
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        if(x < 10) {
+            return true;
+        }
+        String xOfString = Integer.toString(x);
+        for(int i = 0; i < xOfString.length() / 2; i++) {
+            if(xOfString.charAt(i) != xOfString.charAt(xOfString.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
