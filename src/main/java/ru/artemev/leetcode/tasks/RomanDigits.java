7c9d5594eec48acbbe0926da96ef7778dfe4fc17
nialliv <artemjevnikita@yandex.ru>
2024-01-15 20:03:07 +0400

    added new converter for RomanDigits

--- src/main/java/ru/artemev/leetcode/tasks/RomanDigits.java
+++ src/main/java/ru/artemev/leetcode/tasks/RomanDigits.java
@@ -1,5 +1,7 @@
 package ru.artemev.leetcode.tasks;
 
+import java.util.Map;
+
 public class RomanDigits {
     public static String intToRoman(int num) {
         StringBuilder result = new StringBuilder();
@@ -17,5 +19,29 @@ public class RomanDigits {
 
 
         return result.toString();
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+
+        Map<Character, Integer> m = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+        for (int i = 0; i < s.length(); i++) {
+            if(i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                result -= m.get(s.charAt(i));
+            } else {
+                result += m.get(s.charAt(i));
+            }
+        }
+
+        return result;
     }
 }
