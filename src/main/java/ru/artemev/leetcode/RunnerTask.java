d7d1d7cab2be898720f721fe5985b0507a338453
nialliv <artemjevnikita@yandex.ru>
2024-04-23 19:07:55 +0400

    refactor

--- src/main/java/ru/artemev/leetcode/RunnerTask.java
+++ src/main/java/ru/artemev/leetcode/RunnerTask.java
@@ -1,9 +1,9 @@
 package ru.artemev.leetcode;
 
+import org.springframework.stereotype.Service;
+
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
