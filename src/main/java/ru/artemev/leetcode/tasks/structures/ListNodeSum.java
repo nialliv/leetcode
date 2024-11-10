a8645e8da85fd853cf805d842aa1e8c512f110e7
nialliv <artemjevnikita@yandex.ru>
2024-11-10 17:05:40 +0400

    added other solution

--- src/main/java/ru/artemev/leetcode/tasks/structures/ListNodeSum.java
+++ src/main/java/ru/artemev/leetcode/tasks/structures/ListNodeSum.java
@@ -3,53 +3,62 @@ package ru.artemev.leetcode.tasks.structures;
 import ru.artemev.leetcode.tasks.helpers.ListNode;
 
 public class ListNodeSum {
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        ListNode tmp = result;
-        int tmpVal;
-        while (l1 != null || l2 != null) {
-            if (l1 != null && l2 != null) {
-                tmpVal = l1.val + l2.val + tmp.val;
-                if (tmpVal >= 10) {
-                    tmp.val = tmpVal % 10;
-                    tmp.next = new ListNode(1);
-                } else {
-                    tmp.val = tmpVal;
-                    if (l1.next != null || l2.next != null) {
-                        tmp.next = new ListNode();
-                    }
-                }
-                tmp = tmp.next;
-                l2 = l2.next;
-                l1 = l1.next;
-            } else if (l1 != null) {
-                tmpVal = l1.val + tmp.val;
-                if (tmpVal >= 10) {
-                    tmp.val = tmpVal % 10;
-                    tmp.next = new ListNode(1);
-                } else {
-                    tmp.val = tmpVal;
-                    if (l1.next != null) {
-                        tmp.next = new ListNode();
-                    }
-                }
-                l1 = l1.next;
-                tmp = tmp.next;
-            } else {
-                tmpVal = l2.val + tmp.val;
-                if (tmpVal >= 10) {
-                    tmp.val = tmpVal % 10;
-                    tmp.next = new ListNode(1);
-                } else {
-                    tmp.val = tmpVal;
-                    if (l2.next != null) {
-                        tmp.next = new ListNode();
-                    }
-                }
-                l2 = l2.next;
-                tmp = tmp.next;
-            }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode pointer1 = l1;
+        ListNode pointer2 = l2;
+        int tmpVal = 0;
+        int firstVal = l1.val + l2.val;
+        if(firstVal > 9 ) {
+            result.val = firstVal % 10;
+            tmpVal = 1;
+        } else {
+            result.val = firstVal;
+        }
+        ListNode pointerResult = result;
+        while (pointer1.next != null || pointer2.next != null) {
+            pointerResult.next = new ListNode();
+            if (pointer1.next == null) {
+                int val = pointer2.next.val + tmpVal;
+                if (val > 9) {
+                    pointerResult.next.val = val % 10;
+                    tmpVal = 1;
+                } else {
+                    pointerResult.next.val = val;
+                    tmpVal = 0;
+                }
+                pointer2 = pointer2.next;
+                pointerResult = pointerResult.next;
+                continue;
+            }
+            if (pointer2.next == null) {
+                int val = pointer1.next.val + tmpVal;
+                if (val > 9) {
+                    pointerResult.next.val = val % 10;
+                    tmpVal = 1;
+                } else {
+                    pointerResult.next.val = val;
+                    tmpVal = 0;
+                }
+                pointer1 = pointer1.next;
+                pointerResult = pointerResult.next;
+                continue;
+            }
+            int val = pointer1.next.val + pointer2.next.val + tmpVal;
+            if (val > 9) {
+                pointerResult.next.val = val % 10;
+                tmpVal = 1;
+            } else {
+                pointerResult.next.val = val;
+                tmpVal = 0;
+            }
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+            pointerResult = pointerResult.next;
+        }
+        if(tmpVal > 0) {
+            pointerResult.next = new ListNode(1);
         }
         return result;
     }
---
+++ src/test/java/ru/artemev/leetcode/tasks/structures/ListNodeSumTest.java
@@ -0,0 +1,44 @@
+package ru.artemev.leetcode.tasks.structures;
+
+import org.junit.jupiter.api.Test;
+import ru.artemev.leetcode.tasks.helpers.ListNode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ListNodeSumTest {
+
+    @Test
+    void addTwoNumbers_test1() {
+//        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+//        ListNode l2  = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode expected = buildList(new int[]{7, 0, 8});
+        ListNode actual = ListNodeSum.addTwoNumbers(buildList(new int[]{2, 4, 3}), buildList(new int[]{5, 6, 4}));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addTwoNumbers_test2() {
+        ListNode l1 = new ListNode(0);
+        ListNode expected = new ListNode(0);
+        assertEquals(expected, ListNodeSum.addTwoNumbers(l1, l1));
+    }
+
+    @Test
+    void addTwoNumbers_test3() {
+        ListNode l1 = buildList(new int[]{9, 9, 9, 9, 9, 9, 9});
+        ListNode l2 = buildList(new int[]{9, 9, 9, 9});
+        ListNode expected = buildList(new int[]{8, 9, 9, 9, 0, 0, 0, 1});
+        assertEquals(expected, ListNodeSum.addTwoNumbers(l1, l2));
+    }
+
+    private ListNode buildList(int[] nums) {
+        ListNode result = new ListNode();
+        ListNode pRes = result;
+        pRes.val = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pRes.next = new ListNode(nums[i]);
+            pRes = pRes.next;
+        }
+        return result;
+    }
+}
