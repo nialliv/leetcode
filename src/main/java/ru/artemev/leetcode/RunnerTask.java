08840a245c48f409749663affb40e996109d124a
nialliv <artemjevnikita@yandex.ru>
2025-01-30 22:02:46 +0400

    added something

--- src/main/java/ru/artemev/leetcode/RunnerTask.java
+++ src/main/java/ru/artemev/leetcode/RunnerTask.java
@@ -2,16 +2,41 @@ package ru.artemev.leetcode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RunnerTask {
 
+    private final Random random = new Random();
+
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
+        log.info("I'm Started");
+        Map<Character, List<String>> words = Stream.generate(this::getRandomWord)
+                .limit(1000)
+                .collect(Collectors.groupingBy(word -> word.charAt(0)));
+
+        words.computeIfAbsent('0', k -> new ArrayList<>()).add("value");
+        MapUtils.debugPrint(System.out, "words", words);
+    }
+
+    private String getRandomWord() {
+        char[] word = new char[random.nextInt(8) + 3];
+        for (int i = 0; i < word.length; i++) {
+            word[i] = (char) ('a' + random.nextInt(26));
+        }
+        return new String(word);
     }
 }
--- src/main/java/ru/artemev/leetcode/tasks/algorithms/ContainerWithMostWater.java
+++ src/main/java/ru/artemev/leetcode/tasks/algorithms/ContainerWithMostWater.java
@@ -2,19 +2,33 @@ package ru.artemev.leetcode.tasks.algorithms;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
+
+
+        List<Integer> integers = List.of(1, 2, 3);
+
+        List<Integer> list = new ArrayList<>(integers.stream()
+                .filter(a -> true)
+                .toList());
+
+        list.addAll(List.of(5, 6, 7));
+
+
         int left = 0;
         int right = height.length - 1;
         int max = 0;
 
-        while(left < right) {
+        while (left < right) {
             int tmp = Math.min(height[left], height[right]) * (right - left);
-            if(max < tmp) max = tmp;
+            if (max < tmp) max = tmp;
 
-            if(height[left] < height[right]) {
+            if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
