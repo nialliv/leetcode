1b2c4c0c6e3d10c16e9d9e72dff80ceba4b46721
nialliv <artemjevnikita@ya.ru>
2025-08-18 17:57:41 +0400

    refactor

--- src/test/java/ru/artemev/leetcode/tasks/structures/MergeToListsTest.java
+++ src/test/java/ru/artemev/leetcode/tasks/structures/MergeToListsTest.java
@@ -1,7 +1,5 @@
 package ru.artemev.leetcode.tasks.structures;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.artemev.leetcode.tasks.helpers.ListNode;
